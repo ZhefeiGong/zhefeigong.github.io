ceca504599e8f3427e91b70191aa339cb7ba40cd
zhefeigong <zhefeigong@gmail.com>
2024-03-24 22:33:39 +0900

    update

--- map/test/index.js
+++ map/test/index.js
@@ -52,8 +52,6 @@
   
 //   window.initMap = initMap;
 
-
-
 // This example displays a marker at the center of Australia.
 // When the user clicks the marker, an info window opens.
 function initMap() {
