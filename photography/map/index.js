305325a2b203881e2ace64c71a931f1461b5ac96
zhefeigong <zhefeigong@gmail.com>
2024-03-25 01:03:20 +0900

    update

--- photography/map/index.js
+++ photography/map/index.js
@@ -10,27 +10,27 @@ function initMap() {
       {
         position: { lat: 34.8791806, lng: -111.8265049 },
         title: "Boynton Pass",
-        picurl: "/assets/ori/test_200x200.png",
+        picurl: "/assets/ori/test2_200x200.png",
       },
       {
         position: { lat: 34.8559195, lng: -111.7988186 },
         title: "Airport Mesa",
-        picurl: "/assets/ori/test_200x200.png",
+        picurl: "/assets/ori/test2_200x200.png",
       },
       {
         position: { lat: 34.832149, lng: -111.7695277 },
         title: "Chapel of the Holy Cross",
-        picurl: "/assets/ori/test_200x200.png",
+        picurl: "/assets/ori/test2_200x200.png",
       },
       {
         position: { lat: 34.823736, lng: -111.8001857 },
         title: "Red Rock Crossing",
-        picurl: "/assets/ori/test_200x200.png",
+        picurl: "/assets/ori/test2_200x200.png",
       },
       {
         position: { lat: 34.800326, lng: -111.7665047 },
         title: "Bell Rock",
-        picurl: "/assets/ori/test_200x200.png",
+        picurl: "/assets/ori/test_150x150.png",
       },
     ];
     // Create an info window to share between markers.
